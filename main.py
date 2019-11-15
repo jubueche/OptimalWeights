f59322639cafd0bdab39e7f21ba52ffecfd63c0a
Julian Buechel <julianb@ini.uzh.ch>
2019-11-15 09:27:26 +0100

    Added concurrency

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import asyncio
 
-
 def round_sum(x,N_sum):
 	sum = 0
 	for n in range(1,N_sum+1):
@@ -37,42 +36,58 @@ def get_loss(lam1,A,m,w):
 	return lam1/2*np.linalg.norm(np.matmul(round_sum(A,100),m)-w,2)**2
 
 
-def prune(A,m,w,w_hat):
+def prune(A,m,w,w_hat,N,fan_in):
 	A = np.round(A)
 
 	exc_syn_weight = max(m)
 	inh_syn_weight = min(m)
 	exc_syn_index = np.argmax(m)
 	inh_syn_index = np.argmin(m)
-
+	count = 0
 	for i in range(A.shape[0]): #! Wrap evth in while changed smth loop
 		# Check if we can remove an excitatory weight to improve the difference
 		changed_smth = True
+		if(i % N == 0):
+			count = np.sum(np.abs(A[i:i+N,:])) # Count has to stay below 64
+			if(count > 64):
+				print("Error: More than fan-in number of weights.")
+				changed_smth = False
+			equal_share = int((fan_in-count) / N)
+			
+		per_neuron_cnt = 0
 		while(changed_smth):
 			changed_smth = False
 			if(A[i,exc_syn_index] >0 and abs(w[i]-w_hat[i]) >= abs(w[i]-(w_hat[i]-exc_syn_weight))):
-				print("Removed excitatory synapse. w: %.3f w_hat: %.3f Exc.syn.weight: %.3f" % (w[i],w_hat[i],exc_syn_weight))
+				#print("Removed excitatory synapse. w: %.3f w_hat: %.3f Exc.syn.weight: %.3f" % (w[i],w_hat[i],exc_syn_weight))
 				w_hat[i] -= exc_syn_weight
 				A[i,exc_syn_index] -= 1
 				changed_smth = True
+				count -= 1
+				per_neuron_cnt -= 1
 			# Check if we can remove an inhibitory synaps
 			elif(A[i,inh_syn_index] >0 and abs(w[i]-w_hat[i]) >= abs(w[i]-(w_hat[i] + abs(inh_syn_weight)))):
-				print("Removed inhibitory synapse. w: %.3f w_hat: %.3f Inh.syn.weight: %.3f" % (w[i],w_hat[i],inh_syn_weight))
+				#print("Removed inhibitory synapse. w: %.3f w_hat: %.3f Inh.syn.weight: %.3f" % (w[i],w_hat[i],inh_syn_weight))
 				w_hat[i] += abs(inh_syn_weight)
 				A[i,inh_syn_index] -= 1
 				changed_smth = True
+				count -= 1
+				per_neuron_cnt -= 1
 			# Check if we can add an excitatory weight
-			elif(abs(w[i]-w_hat[i]) > abs(w[i] - (w_hat[i]+exc_syn_weight))):
-				print("Added excitatory synapse. w: %.3f w_hat: %.3f Exc.syn.weight: %.3f" % (w[i],w_hat[i],exc_syn_weight))
+			elif(abs(w[i]-w_hat[i]) > abs(w[i] - (w_hat[i]+exc_syn_weight)) and count < fan_in and per_neuron_cnt < equal_share):
+				#print("Added excitatory synapse. w: %.3f w_hat: %.3f Exc.syn.weight: %.3f" % (w[i],w_hat[i],exc_syn_weight))
 				w_hat[i] += exc_syn_weight
 				A[i,exc_syn_index] += 1
 				changed_smth = True
+				count += 1
+				per_neuron_cnt += 1
 			# Check if we can add an inhibitory synapse
-			elif(abs(w[i]-w_hat[i]) > abs(w[i] - (w_hat[i]+inh_syn_weight))):
-				print("Added inhibitory synapse. w: %.3f w_hat: %.3f Exc.syn.weight: %.3f" % (w[i],w_hat[i],inh_syn_weight))
+			elif(abs(w[i]-w_hat[i]) > abs(w[i] - (w_hat[i]+inh_syn_weight)) and count < fan_in and per_neuron_cnt < equal_share):
+				#print("Added inhibitory synapse. w: %.3f w_hat: %.3f Exc.syn.weight: %.3f" % (w[i],w_hat[i],inh_syn_weight))
 				w_hat[i] += inh_syn_weight
 				A[i,inh_syn_index] += 1
 				changed_smth = True
+				count += 1
+				per_neuron_cnt += 1
 	
 	return A,w_hat
 
@@ -83,69 +98,90 @@ def prune(A,m,w,w_hat):
 #plt.show()
 
 #np.random.seed(42)
-np.set_printoptions(precision=2, suppress=True)
+np.set_printoptions(precision=5, suppress=True)
 
-def learn(N,N_sum,T,lam1,lam2,alpha,w,m,A):
+async def learn(N,N_sum,T,lam1,lam2,alpha,w,m,A,fan_in):
 	for i in range(T):
 		loss = get_loss(lam1,A,m,w)
 		if(i % 2 == 0):
 			m = m - alpha*get_gradient_m_sum(lam1,A,w,m,N_sum)
 		else:
 			A = A - alpha*get_gradient_A_sum(lam1,A,m,w,N_sum)
-		print("Scaled loss is %.4f" % (loss/N**2))
+		#print("Scaled loss is %.6f" % (loss/N**2))
 
-	if((m[0] > 0 and m[1] > 0) or (m[0] < 0 and m[1] < 0)):
-		return_loss = np.inf
+		
+	M = round_sum(A,N_sum)
+	M = np.round(M)
+
+	if((m[0] >= 0 and m[1] <= 0) or (m[0] <= 0 and m[1] >= 0) and (M >= 0).all()):
+		m = np.linalg.lstsq(M,w,rcond=None)[0]
+		w_hat = np.matmul(M,m)
+		M,w_hat = prune(M,m,w,w_hat,N,fan_in)
+		final_error = np.linalg.norm(w-w_hat,2)/N**2
 	else:
-		return_loss = loss/N**2
+		final_error = np.inf
+	print(final_error)
 
-	return A,m,return_loss
+	return M,m,final_error
 
-N = 2
-N_sum = 100
-T = 1000
-lam1 = 1
-lam2 = 1
-alpha = 0.001
-w = np.random.randn(N**2).reshape((-1,1))
-m = np.random.randn(2).reshape((-1,1))
-A = np.abs(np.random.randn(N**2,2))
+async def naive(N,w,fan_in):
+	number_conns_per_neuron = int(fan_in / N)
+	
 
-A,_,loss = learn(N,N_sum,T,lam1,lam2,alpha,w,m,A)
+async def main():
 
-M = round_sum(A,N_sum)
+	N = 20
+	N_sum = 100
+	T = 500
+	lam1 = 1
+	lam2 = 1
+	alpha = 0.001
+	fan_in = 512
+	w = np.random.randn(N**2).reshape((-1,1))
+	m = np.random.randn(2).reshape((-1,1))
+	A = np.abs(np.random.randn(N**2,2))
 
-print("M is")
-print(M)
-M = np.round(M)
+	num_tasks = 20
+	tasks = []
+	for t in range(num_tasks):
+		#m = np.random.randn(2).reshape((-1,1))
+		m = np.asarray([min(w),max(w)]).reshape((-1,1))
+		A = np.abs(np.random.randn(N**2,2))
+		tasks.append(asyncio.create_task(learn(N,N_sum,T,lam1,lam2,alpha,w,m,A,fan_in)))
 
-assert loss < np.inf, "Have not found a valid pair of weights"
-assert (M >= 0).all(), "Found negative entry in M matrix"
+	final_errors = np.zeros(num_tasks)
+	Ms = []
+	ms = []
+	for idx,task in enumerate(tasks):
+		M,m,loss = await task
+		final_errors[idx] = loss
+		Ms.append(M)
+		ms.append(m)
 
-print("Final loss is %.4f" % loss)
+	min_idx = np.argmin(final_errors)
+	M = Ms[min_idx]
+	m = ms[min_idx]
+	print("Weights are"); print(m)
+	
+	M_res = np.sum(M, axis=1).reshape((N,N))
+	print(M_res.astype(np.int))
 
-# Finally, use computed A and least squares to compute needed synaptic weights w = A*m
-m = np.linalg.lstsq(M,w,rcond=None)[0]
 
+	print(final_errors)
+	w_hat = np.matmul(M,m)
+	w = w.reshape((N,N))
+	w_hat = w_hat.reshape((N,N))
+	print("True weights: "); print(w)
+	print("Reconstructed weights: "); print(w_hat)
 
-print("m is")
-print(m)
-print("Reconstructed weights")
-w_hat = np.matmul(M,m)
-print(w_hat)
-print("True weights")
-print(w)
+	plt.figure(figsize=(5,10))
+	plt.subplot(211)
+	plt.matshow(w,fignum=False, vmin=np.min(w),vmax=np.max(w))
+	plt.colorbar()
+	plt.subplot(212)
+	plt.matshow(w_hat,fignum=False, vmin=np.min(w),vmax=np.max(w))
+	plt.colorbar()
+	#plt.tight_layout()
+	plt.show()
 
-A,w_hat = prune(M,m,w,w_hat)
-
-print("After pruning:")
-print(A)
-
-print("Reconstructed weights")
-print(w_hat)
-
-print("True weights")
-print(w)
-
-final_error = np.linalg.norm(w-w_hat,2)/N**2
-print("Final error is %.5f" % final_error)
+asyncio.run(main())
